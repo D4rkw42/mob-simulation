353cb22e1df6e81450ea44992105295b1fbef778
D4rkw42 <william.conceicao042@gmail.com>
2024-07-26 00:48:03 -0300

    Commit Inicial

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <SDL2/SDL.h>
+#undef main
+
+int main(int argc, char* argv[]) {
+   SDL_Window* window = NULL;
+
+    /*
+     * Inicialização e criação da janela
+     */
+    if ( SDL_Init(SDL_INIT_VIDEO) == 0 )
+        window = SDL_CreateWindow("Tutorial SDL 2", 50, 50, 640, 480, 0);
+    else
+        SDL_Log("Erro na inicialização: %s", SDL_GetError());
+
+    /*
+     * Main loop
+     */
+    if ( window != NULL )
+        while( !SDL_QuitRequested() ) { /* ... */ }
+    else
+        SDL_Log("Erro na criação da janela: %s", SDL_GetError());
+
+    /*
+     * Finialização
+     */
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
