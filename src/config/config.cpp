62a36addab513aebcbd87a9bdf7304c6f7cbee9b
D4rkw42 <william.conceicao042@gmail.com>
2024-08-10 21:03:12 -0300

    Python file tool
    Adiciona uma ferramenta para agilizar comandos

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -4,10 +4,12 @@
 
 #include "config.hpp"
 
+#include <cstdlib>
+
 // configuração
 const char* APPLICATION_NAME = "Mob Simulation";
 
-const siv::PerlinNoise::seed_type seed = 123456u;
+const siv::PerlinNoise::seed_type seed = time(0);
 
 // world definitions
 const char* WORLD_CONFIG_FILE = "assets/world.json"; // arquivo de configuração do mundo
---
+++ tools.py
@@ -0,0 +1,46 @@
+# tool kit para desenvolvimento de projetos cmake - by D4rkw42
+
+import sys, os
+import subprocess
+
+application = "mob-simulation.exe"
+
+# flushing
+os.system("cls")
+
+# comandos listados
+command_list = ("run", "build")
+commands = []
+
+# comandos
+
+def run_command(*args): # executa o aplicativo
+    target = sys.path[0] + "\\build\\" + application
+
+    print(f"[Tools.py] Executando {application}...")
+    subprocess.call([target])
+
+def makefile_gen_command(*args): # gera o makefile (para arquivos novos)
+    print("[Tools.py] Gerando Makefile com CMake...")
+    os.system("cmake . -B build")
+
+def build_command(*args): # faz a build do aplicativo
+    print(f"[Tools.py] Gerando {application}...")
+    os.system("cmake --build build")
+
+# adicionando comandos
+commands.append(("run", run_command))
+commands.append(("makefile", makefile_gen_command))
+commands.append(("build", build_command))
+
+#
+
+if __name__ == "__main__":
+    args = sys.argv
+
+    # executando os comandos corretos
+    for cmd in commands:
+        name = args[1]
+
+        if cmd[0] == name:
+            cmd[1](args)
